0ce72614a87cd67e3f5bd5eda2edd5818f1124d8
Stephanie Ryan <sryan@catalyte.io>
2021-08-13 08:49:41 -0500

    add book-slider component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookSliderComponent } from './book-slider/book-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookSliderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/book-slider/book-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-slider',
+  templateUrl: './book-slider.component.html',
+  styleUrls: ['./book-slider.component.css']
+})
+export class BookSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
