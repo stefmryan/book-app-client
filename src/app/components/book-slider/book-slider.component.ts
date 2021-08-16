e921f5eeb8f38979f779f96a1f5d3f9e5271385d
Stephanie Ryan <sryan@catalyte.io>
2021-08-16 16:55:59 -0500

    work on logic for closeBook method

--- src/app/components/book-slider/book-slider.component.ts
+++ src/app/components/book-slider/book-slider.component.ts
@@ -29,6 +29,12 @@ export class BookSliderComponent implements OnInit {
   isTransformPrevBtn: boolean = false;
   isTransformNextBtn: boolean = false;
 
+  //isAtBeginning: boolean = false;
+
+  moveBookDiv: string = "";
+
+
+
   openBook(){
 this.isTransformBookDiv = true;
 this.isTransformPrevBtn = true;
@@ -36,7 +42,11 @@ this.isTransformNextBtn = true;
   }
 
   closeBook(isAtBeginning:boolean){
-console.log(isAtBeginning);
+    if(isAtBeginning){
+      this.moveBookDiv = "translateX(0%)"
+    }else{
+      this.moveBookDiv = "translateX(100%)"
+    }
   }
 
   goNextPage(){
@@ -45,16 +55,13 @@ console.log(isAtBeginning);
         case 1:
           this.openBook();
           this.isFlipped1 = true;
-          //this.changeZIndex1 = true;
           break;
         case 2:
           this.isFlipped2 = true;
-          //this.changeZIndex2 = true;
           break;
         case 3:
           this.isFlipped3 = true;
-          //this.changeZIndex3 = true;
-          this.closeBook(true);
+          this.closeBook(false);
           break;
         default:
           throw new Error("unknown state");
@@ -66,7 +73,24 @@ console.log(isAtBeginning);
 
   goPrevPage(){
     if(this.currentLocation > 1){
-      this.isFlipped1 = false;
+      switch(this.currentLocation) {
+        case 2:
+          this.isTransformBookDiv = false;
+          // this.isTransformNextBtn = false;
+          // this.isTransformPrevBtn = false;
+          this.closeBook(true);
+          this.isFlipped1 = false;
+          break;
+        case 3:
+          this.isFlipped2 = false;
+          break;
+        case 4:
+          this.openBook();
+          this.isFlipped3 = false;
+          break;
+        default:
+          throw new Error('unknown state');
+      }
       this.currentLocation--;
       console.log("pressed prev btn")
     }
