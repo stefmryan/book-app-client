a93203890110cafe6d6efe777e8fa82202d4842a
Stephanie Ryan <sryan@catalyte.io>
2021-08-16 14:53:39 -0500

    add logic to add classes and styles dynamically to bookslider

--- src/app/components/book-slider/book-slider.component.ts
+++ src/app/components/book-slider/book-slider.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { faArrowAltCircleLeft, faArrowAltCircleRight } from '@fortawesome/free-regular-svg-icons';
 
 
 import { BooksService } from 'src/app/services/books.service';
 import { Book } from 'src/app/Book';
+
 
 @Component({
   selector: 'app-book-slider',
@@ -15,6 +16,54 @@ export class BookSliderComponent implements OnInit {
 
   faArrowAltCircleLeft = faArrowAltCircleLeft;
   faArrowAltCircleRight = faArrowAltCircleRight;
+
+  currentLocation:number = 1;
+  numOfPapers: number= 3;
+  maxLocation:number = this.numOfPapers + 1;
+
+  isFlipped1: boolean = false;
+  isFlipped2: boolean = false;
+  isFlipped3: boolean = false;
+
+  openBook(){
+
+  }
+  closeBook(isAtBeginning:boolean){
+console.log(isAtBeginning);
+  }
+
+  goNextPage(){
+    if(this.currentLocation < this.maxLocation){
+      switch(this.currentLocation){
+        case 1:
+          this.openBook();
+          this.isFlipped1 = true;
+          //this.changeZIndex1 = true;
+          break;
+        case 2:
+          this.isFlipped2 = true;
+          //this.changeZIndex2 = true;
+          break;
+        case 3:
+          this.isFlipped3 = true;
+          //this.changeZIndex3 = true;
+          this.closeBook(true);
+          break;
+        default:
+          throw new Error("unknown state");
+      }
+      this.currentLocation++;
+      console.log("next btn pressed");
+    }
+  }
+
+  goPrevPage(){
+    if(this.currentLocation > 1){
+      this.isFlipped1 = false;
+      this.currentLocation--;
+      console.log("pressed prev btn")
+    }
+  }
 
   constructor(private bookService: BooksService) { }
 
